d08899a35bd7134af17617e87d1f220ebef45b0c
hindupriyaBoddu <147792468+hindupriyaBoddu@users.noreply.github.com>
2023-10-28 13:04:48 +0530

    #include<stdio.h> int main() {     int X,Y;     scanf("%d%d",&X,&Y);     if(5*X>=Y)     {         printf("YES");     }     else     {         printf("NO");      }     }

---
+++ Reach_Home.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d%d",&X,&Y);
+    if(5*X>=Y)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");    
+     }
+    }
+
