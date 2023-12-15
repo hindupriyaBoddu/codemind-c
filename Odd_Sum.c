e50bfa55e9c1f87494bc427790ec61e77d472722
hindupriyaBoddu <147792468+hindupriyaBoddu@users.noreply.github.com>
2023-12-15 11:23:49 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int acet[n],i;     for(i=0;i<n;i++)     {         scanf("%d",&acet[i]);     }     int sum=0;     for(i=0;i<n;i++)     {         if(acet[i]%2==1)         {             sum=sum+acet[i];
            }
        }
        printf("%d",sum);
    }

---
+++ Odd_Sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int acet[n],i;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&acet[i]);
+    }
+    int sum=0;
+    for(i=0;i<n;i++)
+    {
+        if(acet[i]%2==1)
+        {
+            sum=sum+acet[i];
+            
+        }
+    }
+    printf("%d",sum);
+}
