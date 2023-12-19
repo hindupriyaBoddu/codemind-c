060df244d60a7cf15b5781031113a5a6cc35d1c4
hindupriyaBoddu <147792468+hindupriyaBoddu@users.noreply.github.com>
2023-12-19 18:43:38 +0530

    #include<stdio.h> int main() {     int X,Y,Z;     scanf("%d%d%d",&X,&Y,&Z);     int a=2*(X+Y)*Z;     printf("%d",a); }

--- Frames.c
+++ Frames.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int l,b,cost,perimeter,uc;
-    scanf("%d%d%d",&l,&b,&uc);
-    perimeter=2*(l+b);
-    cost=perimeter*uc;
-    printf("%d
-",cost);
+    int X,Y,Z;
+    scanf("%d%d%d",&X,&Y,&Z);
+    int a=2*(X+Y)*Z;
+    printf("%d",a);
 }
