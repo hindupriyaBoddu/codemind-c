12195bd44cd81c8ffd4bae1a390bd7cd4836f907
hindupriyaBoddu <147792468+hindupriyaBoddu@users.noreply.github.com>
2023-12-31 10:02:16 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     float avg,sum=0;     for(i=0;i<n;i++)     {         sum=sum+arr[i];         avg=sum/n;
        }
        printf("%.2f",avg);
    }

---
+++ Average_of_the_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    float avg,sum=0;
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+        avg=sum/n;
+        
+    }
+    printf("%.2f",avg);
+}
