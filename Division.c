eae2e1655d84d3dd4464915f705385903591f3b2
hindupriyaBoddu <147792468+hindupriyaBoddu@users.noreply.github.com>
2024-01-01 21:54:23 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=a/b;     printf("%d",c); }

--- Division.c
+++ Division.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z;
-    scanf("%d%d",&x,&y);
-    z=x/y;
-    printf("%d",z);
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a/b;
+    printf("%d",c);
 }
